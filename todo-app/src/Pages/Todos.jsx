454940cad3e3deb7def864852f81a7b8e700a703
Jhonl117 <jhonedi990223@gmail.com>
2025-12-10 02:12:10 -0500

    feat: formulario para crear nuevos todos con validación básica. JL

--- todo-app/src/Pages/Todos.jsx
+++ todo-app/src/Pages/Todos.jsx
@@ -1,25 +1,53 @@
 import { useEffect, useState } from "react";
-
+const [nuevoTodo, setNuevoTodo] = useState("");
 export default function Todos() {
 
-  const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then(res => res.json())
-      .then(data => setTodos(data));
-  }, []);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos?_limit=10")
+            .then(res => res.json())
+            .then(data => setTodos(data));
+    }, []);
 
-  return (
-    <div>
-    <h1>Listado de TODOS</h1>
-    <ul>
-      {todos.map(todo => (
-        <li key={todo.id}>
-          {todo.title} - {todo.completed ? "✔" : "❌"}
-        </li>
-      ))}
-    </ul>
-  </div>
-  );
+    return (
+        <div>
+            <h1>Listado de TODOS</h1>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        {todo.title} - {todo.completed ? "✔" : "❌"}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+<form onSubmit={handleSubmit}>
+    <input
+        type="text"
+        value={nuevoTodo}
+        onChange={(e) => setNuevoTodo(e.target.value)}
+        placeholder="Nuevo TODO"
+    />
+    <button type="submit">Agregar</button>
+</form>
+
+function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!nuevoTodo.trim()) {
+        alert("El campo no puede estar vacío");
+        return;
+    }
+
+    const nuevo = {
+        id: Date.now(),
+        title: nuevoTodo,
+        completed: false
+    };
+
+    setTodos([nuevo, ...todos]);
+    setNuevoTodo("");
 }
